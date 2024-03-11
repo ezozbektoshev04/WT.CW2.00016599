cb7a3ddc7d240f6af227ef7b4f855a71bcf0f0ee
ezozbektoshev04 <toshevezozbek04@gmail.com>
2024-03-11 11:22:20 +0500

    Project started serving view side

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 // express web app instance
 const express = require("express");
-
 // parse request body to json
 const body_parser = require("body-parser");
-
 // for File IO
 const path = require("path");
+
+// for web routing
+const web_route = require("./routes/web");
 
 // make mock database (raw .json file) available globally in app
 global.mock_db = path.join(__dirname, "./data/mock_db.json");
 
 const app = express();
+// Set the view engine for web routes
+app.set("view engine", "pug");
 
-app.get("/", function (req, res) {
-  return res.json({
-    message: "hello world",
-  });
-});
+app.use("/css", express.static("public/css"));
+app.use("/js", express.static("public/js"));
 
-const port = 3000;
+app.use("/", web_route); // web routes
+const port = 4000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
