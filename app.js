9ddaaab12291dd26e0bc25194b9e060a84730d66
ezozbektoshev04 <toshevezozbek04@gmail.com>
2024-03-11 13:06:36 +0500

    Add and Delete backend side is done

--- app.js
+++ app.js
@@ -5,19 +5,28 @@ const body_parser = require("body-parser");
 // for File IO
 const path = require("path");
 
-// for web routing
-const web_route = require("./routes/web");
-
-// make mock database (raw .json file) available globally in app
 global.mock_db = path.join(__dirname, "./data/mock_db.json");
 
+const web_route = require("./routes/web");
+const api_route = require("./routes/api");
+
 const app = express();
+
 // Set the view engine for web routes
 app.set("view engine", "pug");
-
 app.use("/css", express.static("public/css"));
 app.use("/js", express.static("public/js"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", api_route); // API routes
 app.use("/", web_route); // web routes
+
+// redirect to home page if unknown requests requested
+app.use((req, res) => {
+  res.redirect("/");
+});
+
 const port = 4000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
